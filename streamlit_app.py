642459cef146019ace1a6effa9345b2ae49ef2e1
Nikhil YS <nikhil.nyk007@gmail.com>
2023-07-06 12:16:20 +0530

    Update streamlit_app.py
    added snowflake connectivity

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,14 @@ st.title("Zena's Amazing Athleisure Catalog")
 selection=st.selectbox('Pick a sweatsuit color or style:', ('Orange', 'Burgandy'))
 
 st.write('You have selected: ',selection)
+
+
+my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur=my_cnx.cursor()
+
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_REGION(), CURRENT_ACCOUNT()")
+
+my_data_row=my_cur.fetchone()
+
+st.text("Hello From Snowflake")
+st.text(my_data_row)
