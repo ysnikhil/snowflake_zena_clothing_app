d0611da0d60bbdd8c55bb0212ea2d10748e4a21d
Nikhil YS <nikhil.nyk007@gmail.com>
2023-07-10 23:24:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ selection=st.selectbox('Pick a sweatsuit color or style:', df['color_or_style'].
 st.write("You selected: ", selection)
 
 product_caption='Our Warm, Comfortable, '+ selection+ ' SweatSuit!'
-#st.write(df[df['color_or_style']==selection]['direct_url'])
+st.write(df[df['color_or_style']==selection]['direct_url'])
 #st.write(df['direct_url'])
 st.image(df[df['color_or_style']==selection]['direct_url'][0], width=400, caption=product_caption)
 
